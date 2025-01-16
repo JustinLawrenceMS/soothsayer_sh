8255428284f18fc1eeb9c5d36c2b4836df050956
Justin Lawrence <justinlawrence@Justins-Mac-mini.local>
2025-01-16 08:25:16 -0600

    refactor - use appropriate method names

--- soothsayer.sh
+++ soothsayer.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-brew bundle > /dev/null 
-shampoozee() {
+brew bundle >/dev/null
+display_menu() {
 	echo "These are the fortunetellers:"
 	echo "1. Don Quixote"
 	echo "2. The Rubaiyyat"
 }
-read -p "What is your name: " name;
-read -p "What knowledge do you seek? " saught;
-shampoozee;
-read -p "Which fortuneteller do you want to use? " fortuneteller
-if [ $fortuneteller == "1" ]; then
-	sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/donquixote/sentences?sentences=1);
-	fortune=$(jq -r '.ipsum_text' <<< "$sooth")
+recurse() {
+	read -p "Which fortuneteller do you want to use? " fortuneteller
+	if [ $fortuneteller == "1" ]; then
+	sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/donquixote/sentences?sentences=1)
+	fortune=$(jq -r '.ipsum_text' <<<"$sooth")
 elif [ $fortuneteller == "2" ]; then
-	sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/sooths/fortune);
-	fortune=$(jq -r '.data.sooth' <<< "$sooth")
+	sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/sooths/fortune)
+	fortune=$(jq -r '.data.sooth' <<<"$sooth")
 else
-	echo "invalid option";
-	read -p "Which fortuneteller do you want to use? " fortuneteller;
+	echo "invalid option"
+	recurse
 fi
-printf "\n\n Hello, $name, \n\n $saught \n\n $fortune \n\n";
+}
+read -p "What is your name: " name
+read -p "What knowledge do you seek? " saught
+display_menu
+recurse
+printf "\n\n Hello, $name, \n\n $saught \n\n $fortune \n\n"
