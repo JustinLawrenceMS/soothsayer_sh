39db36ce03406d50db5266e24c4b6b8f80d2b92d
Justin Lawrence <43936909+JustinLawrenceMS@users.noreply.github.com>
2025-01-16 07:13:47 -0600

    Update soothsayer.sh API url

--- soothsayer.sh
+++ soothsayer.sh
@@ -2,6 +2,6 @@
 brew bundle
 read -p "What is your name: " name;
 read -p "What knowledge do you seek? " saught;
-sooth=$(curl -s https://soothsayer-api.com/api/v1/sooths/fortune);
+sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/sooths/fortune);
 fortune=$(jq -r '.data.sooth' <<< "$sooth")
 printf "\n\n $saught \n\n $fortune \n\n";
