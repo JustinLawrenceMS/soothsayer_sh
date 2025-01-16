2c9a738cf55aae4e80f3bfe7f9641e9479414355
Justin Lawrence <justinlawrence@Justins-Mac-mini.local>
2025-01-16 08:27:13 -0600

    refactor - use appropriate method names

--- soothsayer.sh
+++ soothsayer.sh
@@ -5,21 +5,21 @@ display_menu() {
 	echo "1. Don Quixote"
 	echo "2. The Rubaiyyat"
 }
-recurse() {
+handle_fortuneteller() {
 	read -p "Which fortuneteller do you want to use? " fortuneteller
 	if [ $fortuneteller == "1" ]; then
-	sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/donquixote/sentences?sentences=1)
-	fortune=$(jq -r '.ipsum_text' <<<"$sooth")
-elif [ $fortuneteller == "2" ]; then
-	sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/sooths/fortune)
-	fortune=$(jq -r '.data.sooth' <<<"$sooth")
-else
-	echo "invalid option"
-	recurse
-fi
+		sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/donquixote/sentences?sentences=1)
+		fortune=$(jq -r '.ipsum_text' <<<"$sooth")
+	elif [ $fortuneteller == "2" ]; then
+		sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/sooths/fortune)
+		fortune=$(jq -r '.data.sooth' <<<"$sooth")
+	else
+		echo "invalid option"
+		recurse
+	fi
 }
 read -p "What is your name: " name
 read -p "What knowledge do you seek? " saught
 display_menu
-recurse
+handle_fortuneteller
 printf "\n\n Hello, $name, \n\n $saught \n\n $fortune \n\n"
