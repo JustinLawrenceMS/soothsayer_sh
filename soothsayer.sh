96c0092ba52098c29d411951772df16c054a058d
Justin Lawrence <justinlawrence@Justins-Mac-mini.local>
2025-01-16 08:00:51 -0600

    feature - added fortunteller options

--- soothsayer.sh
+++ soothsayer.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-brew bundle
+brew bundle > /dev/null 
+shampoozee() {
+	echo "These are the fortunetellers:"
+	echo "1. Don Quixote"
+	echo "2. The Rubaiyyat"
+}
 read -p "What is your name: " name;
 read -p "What knowledge do you seek? " saught;
-sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/sooths/fortune);
-fortune=$(jq -r '.data.sooth' <<< "$sooth")
+shampoozee;
+read -p "Which fortuneteller do you want to use? " fortuneteller
+if [ $fortuneteller == "1" ]; then
+	sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/donquixote/sentences?sentences=1);
+	fortune=$(jq -r '.ipsum_text' <<< "$sooth")
+elif [ $fortuneteller == "2" ]; then
+	sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/sooths/fortune);
+	fortune=$(jq -r '.data.sooth' <<< "$sooth")
+else
+	echo "invalid option";
+	read -p "Which fortuneteller do you want to use? " fortuneteller;
+fi
 printf "\n\n Hello, $name, \n\n $saught \n\n $fortune \n\n";
