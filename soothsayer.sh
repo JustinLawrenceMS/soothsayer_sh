7faa68915513a33327480fd4cd83780dfa4495ad
Justin Lawrence <43936909+JustinLawrenceMS@users.noreply.github.com>
2025-01-16 07:15:00 -0600

    Update soothsayer.sh to say the name

--- soothsayer.sh
+++ soothsayer.sh
@@ -4,4 +4,4 @@ read -p "What is your name: " name;
 read -p "What knowledge do you seek? " saught;
 sooth=$(curl -s https://fortune.at.chiclay.com/api/v1/sooths/fortune);
 fortune=$(jq -r '.data.sooth' <<< "$sooth")
-printf "\n\n $saught \n\n $fortune \n\n";
+printf "\n\n Hello, $name, \n\n $saught \n\n $fortune \n\n";
